f220b590c7021f62471b441ce19afb91080f689c
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-06-26 17:09:36 +0530

    modified patterns 9 and 10

--- C++/pattern09.cpp
+++ C++/pattern09.cpp
@@ -5,30 +5,18 @@ int main(){
     int n;
     cin >> n;
 
-    for (int i = 1; i <= n; i++){
-        for (int j = n-i; j > 0; j--){
+    for (int i = -1*n; i <= n; i++){
+        if (i == 0){
+            i++;
+        }
+        for (int j = 1; j <= abs(i)-1; j++){
             cout << " ";
         }
-        for (int j = 1; j <= 2*i-1; j++){
+        for (int j = 1; j <= 2*(n-abs(i))+1; j++){
             cout << "*";
-        }
-        for (int j = n-i; j > 0; j--){
-            cout << " ";
         }
         cout << "\n";
     }
     
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j < i; j++){
-            cout << " ";
-        }
-        for (int j = 2*(n-i)+1; j > 0; j--){
-            cout << "*";
-        }
-        for (int j = 1; j < i; j++){
-            cout << " ";
-        }
-        cout << "\n";
-    }
     return 0;
 }
--- C++/pattern10.cpp
+++ C++/pattern10.cpp
@@ -5,16 +5,9 @@ int main(){
     int n;
     cin >> n;
 
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= i; j++){
-            cout << "* ";
-        }
-        cout << "\n";
-    }
-
-    for (int i = n-1; i > 0; i--){
-        for (int j = 1; j <= i; j++){
-            cout << "* ";
+    for (int i = (n*-1)+1; i <= n-1; i++){
+        for (int j = 1; j <= n-abs(i); j++){
+            cout << "*";
         }
         cout << "\n";
     }
