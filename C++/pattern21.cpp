1d2641e281b9c1bcb321372d40d48529c26ae33a
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-06-27 21:33:31 +0530

    create pattern 21

---
+++ C++/pattern21.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++){
+        
+        if (i == 1 || i == n){
+            for (int j = 1; j <= n; j++){
+                cout << "*";
+            }
+        }
+        else{
+            cout << "*";
+            for (int j = 1; j <= n-2; j++){
+                cout << " ";
+            }
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
