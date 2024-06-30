818073cbbcfc53f5047110d82649dace142fb418
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-06-30 22:04:46 +0530

    create print-1-to-n

--- C++/print-1-to-n.cpp
+++ C++/print-1-to-n.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printNos(int i, int n){
-    if (i <= n){
-        cout << i << " ";
-        printNos(++i,n);
-    }
+int i = 0;
+
+void printNos(int n){
+    if (i > n) return;
+    cout << i++ << " ";
+    printNos(n);
 }
 
 int main(){
     int n;
     cin >> n;
 
-    printNos(1,n);
+    printNos(n);
 
     return 0;
 }
